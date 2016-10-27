800473c87e42878d8e7d155ce659a4b44b9807f9
Fabien JUIF <fabien.juif@zenika.com>
2016-10-27 16:27:22 +0200

    result

--- src/rest/restspread.js
+++ src/rest/restspread.js
@@ -7,3 +7,21 @@ const jobs = {
   morty: 'Student at Harry Herpson High School',
   jerry: 'Advertising agent',
 }
+
+const detail = ({ age, size, lat, long, job }) => `
+  <div>
+    <div>${age} ans</div>
+    <div>${size} cm</div>
+    <div>${lat} / ${long}</div>
+    <div>Métier : ${job}</div>
+  </div>
+`
+
+const personnage = ({ firstName, lastName, ...det }) => `
+  <div>
+    <h2>${firstName} ${lastName}</h2>
+    ${detail(det)}
+  </div>
+`
+
+console.log(personnage({ ...morty, job: jobs.morty }))
